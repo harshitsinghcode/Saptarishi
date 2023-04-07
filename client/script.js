dabd4492e34d74acf45ce5ddffb43a77104b4ec9
Harshit Kumar Singh <110082422+harshitsinghcode@users.noreply.github.com>
2023-04-07 20:20:42 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -90,7 +90,7 @@ const handleSubmit = async (e) => {
 
 //fetch data from server 
 
-const response = await fetch('https://saptarishi.onrender.com',{
+const response = await fetch('https://saptarishie.onrender.com/',{
 method : 'POST',
 headers: {
     'Content-Type': 'application/json'
