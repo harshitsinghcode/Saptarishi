a098fd69d162e728e940ba724b0982126049da96
Harshit Kumar Singh <harshitsinghcode@gmail.com>
2023-04-07 18:27:32 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -90,7 +90,7 @@ const handleSubmit = async (e) => {
 
 //fetch data from server 
 
-const response = await fetch('http://localhost:5000',{
+const response = await fetch('https://saptarishi.onrender.com',{
 method : 'POST',
 headers: {
     'Content-Type': 'application/json'
