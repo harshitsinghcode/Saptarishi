0c27afc9485c0ab586dff4d1b1a49c22921765d9
Harshit Kumar Singh <harshitsinghcode@gmail.com>
2023-05-16 22:54:54 +0530

    mend

--- client/script.js
+++ client/script.js
@@ -1,127 +1,103 @@
-import bot from './assets/bot.svg';
-import user from './assets/user.svg';
+import botImage from './assets/bot.svg';
+import userImage from './assets/user.svg';
 
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
 
+const bot = botImage;
+const user = userImage;
 
-let loadInterval;
+function loader(element) {
+  element.textContent = '';
 
-function loader(element){
-    element.textContent ='';
-
-    loadInterval = setInterval(() => {
-
-        element.textContent+='.';
-
-        if(element.textContent ==='....'){
-            element.textContent='';
-        }
-    }, 300)
-
-}
-
-
-function typeText(element,text){
-    let index =0;
-
-    let interval = setInterval(() => {
-
-        if(index<text.length){
-            element.innerHTML +=text.chartAt(index);
-            index++;
-        }
-        else{
-            clearInterval(interval);
-        }
-    },20)
-}
-
-
-function generateUniqueId(){
-
-    const timestamp =Date.now();
-    const randomNumber=Math.random();
-    const hexadecimalString=randomNumber.toString(16);
-
-    return `id-${timestamp}-${hexadecimalString}`;
-
-}
-
-function chatStripe (isAi, value, uniqueId){
-    return (
-        `
-
-            <div class="wrapper ${isAi && 'ai'}">
-                <div class ="chat">
-                    <div class="profile">
-                        <img
-                            src="${isAi? bot:user}"
-                            alt="${isAi?'bot':'user'}"
-                        />
-                    </div>
-                    <div class="message" id=${uniqueId}>${value}</div>
-                </div>
-            </div>
-   `
-    )
-
-}
-
-const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const data = new FormData(form);
-
-    //user's chats stripe
-
-    chatContainer.innerHTML+=chatStripe(false,data.get('prompt'));
-
-    form.reset();
-
-    const uniqueId = generateUniqueId();
-    chatContainer.innerHTML+=chatStripe(true," ", uniqueId);
-
-    chatContainer.scrollTop=chatContainer.scrollHeight;
-    const messageDiv = document.getElementById(uniqueId);
-
-    loader (messageDiv);
-
-
-//fetch data from server 
-
-const response = await fetch('http://localhost:5000',{
-method : 'POST',
-headers: {
-    'Content-Type': 'application/json'
-},
-body : JSON.stringify({
-    prompt: data.get('prompt')
-})
-})
-
-clearInterval(loadInterval);
-messageDiv.innerHTML='';
-
-
-
-if(response.ok){
-    const data = await response.json();
-    const parsedData = data.bot.trim();
-    // console.log({parsedData})
-    typeText(messageDiv, parsedData);
-}
-else{
-    const err=await response.text();
-    messageDiv.innerHTML="Something Went Wrong!";
-
-    alert(err);
-}
-}
-
-form.addEventListener('submit', handleSubmit)
-form.addEventListener('keyup', (e)=>{
-    if(e.keyCode === 13){
-        handleSubmit(e);
+  const loadInterval = setInterval(() => {
+    element.textContent += '.';
+    if (element.textContent === '....') {
+      element.textContent = '';
     }
-})
+  }, 300);
+
+  return loadInterval;
+}
+
+function typeText(element, text) {
+  let index = 0;
+  const interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+  return `
+    <div class="wrapper ${isAi && 'ai'}">
+      <div class="chat">
+        <div class="profile">
+          <img src="${isAi ? bot : user}" alt="${isAi ? 'bot' : 'user'}">
+        </div>
+        <div class="message" id="${uniqueId}">${value}</div>
+      </div>
+    </div>
+  `;
+}
+
+async function handleChatPrompt(data) {
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+  form.reset();
+
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, '', uniqueId);
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+
+  const loadInterval = loader(messageDiv);
+
+  const response = await fetch('https://saptarishie.onrender.com', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt'),
+    }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+
+  if (response.ok) {
+    const responseData = await response.json();
+    const parsedData = responseData.bot.trim();
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+    messageDiv.innerHTML = 'Something went wrong!';
+    alert(err);
+  }
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  handleChatPrompt(new FormData(form));
+}
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+});
--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
-
+import dotenv from 'dotenv';
 import cors from 'cors';
-import {Configuration, OpenAIApi} from 'openai';
+import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
 
-console.log(process.env.OPENAI_API_KEY)
-const configuration =  new Configuration({
-    apiKey:process.env.OPENAI_API_KEY,
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
@@ -18,33 +16,32 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/',async(req,res) => {
-    res.status(200).send ({
-        message : 'Namaste From Saptarishi',
-    });
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Namaste from Saptarishi',
+  });
 });
 
-app.post('/', async(req, res)=>{
-    try{
-        const prompt = req.body.prompt;
-        const response = await openai.createCompletion({
-            model:"text-davinci-003",
-            prompt: `${prompt}`,
-            temperature:0,
-            max_tokens:3000,
-            top_p:1,
-            frequency_penalty:0.5,
-            presence_penalty:0,
-        });
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
 
-        res.status(200).send({
-            bot:response.data.choices[0].text
-        });
-    } catch(error){
-        console.log(error);
-        res.status(500).send({ error })
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+});
 
-    }
-})
-
-app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
+app.listen(5000, () => console.log('Server is running on http://localhost:5000'));
