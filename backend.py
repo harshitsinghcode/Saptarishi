e464143e783f13a92d887e2178fb7db7909a7f84
harshitsinghcode <harshitsinghcode@gmail.com>
2026-01-17 18:57:09 +0530

    two-file-lango

---
+++ backend.py
@@ -0,0 +1,32 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph.message import add_messages
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGroq(
+    model="llama-3.1-8b-instant",
+    temperature=0.7,
+)
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+checkpointer = MemorySaver()
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
---
+++ frontend.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from langchain_core.messages import HumanMessage
+from backend import chatbot 
+
+st.title("🤖 Llama 3 Chatbot")
+
+CONFIG = {'configurable': {'thread_id': 'thread-1'}}
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.markdown(message['content'])  
+
+user_input = st.chat_input('Type your message here...')
+
+if user_input:
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.markdown(user_input)
+
+   
+    response = chatbot.invoke(
+        {'messages': [HumanMessage(content=user_input)]}, 
+        config=CONFIG
+    )
+    
+    ai_message_content = response['messages'][-1].content
+    
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message_content})
+    with st.chat_message('assistant'):
+        st.markdown(ai_message_content) 
