f609e70f55999b14f42a0af39e76c1008d120eb1
harshitsinghcode <harshitsinghcode@gmail.com>
2026-01-17 19:09:42 +0530

    two-file-lango

--- backend.py
+++ backend.py
@@ -1,7 +1,6 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage
-from langchain_openai import ChatOpenAI
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph.message import add_messages
 from langchain_groq import ChatGroq
